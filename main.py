a3646aeb7ab38c0f59a0ba9c2520b1c86ac82e74
Alejandro Daniel Espinosa Chim <119325061+DanielEspinosaChim@users.noreply.github.com>
2024-03-02 04:17:30 -0600

    ya me tienes hasta la verga

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
+#render template es para renderizar html dede python esto lo hace con la libreria
 app = Flask(__name__)
+items = ['item1', 'item2', 'item3', 'item4', 'item5']   
+@app.route('/index')
+def index():
+    # El objeto request contiene toda la información que viene del cliente.
+    user_ip_information = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip_information)
+    return response
+
+@app.route('/hello')  # Creamos una nueva ruta que no estaba en el programa.
+def hello():  # Nombre de función corregido para mejor claridad
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'items': items
+    }                                               #el context con dos asterscos desempaqueta el diccionario 
+    return render_template('ip_information.html', **context)  # Corregido para que se muestre el valor de user_ip, la prmer avariableantes del igual debe ser igual ue en el html
+
 @app.route('/home')
 
 def HelloWorld():
     return"Hello world, I am doing tests with the fremwork flask"
+
 #para hacer el debugg hay que agregar el siguiente parametro 
 app.run(host='127.0.0.1', port = 81, debug=True)
+
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\MyFirstAplicationWithFlask\venv
+set VIRTUAL_ENV=C:\Users\Personal\Documents\cursoHackathon\myFirstAppWithFlask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
